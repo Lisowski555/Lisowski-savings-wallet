4c5edddf5a4709034c1e55b2da6c31663506e4b6
Lisowski555 <lisowski.rafal555@gmail.com>
2025-06-01 22:24:17 +0200

    wip3

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -9,17 +9,19 @@ const modalStyles: React.CSSProperties = {
     top: 0, left: 0, right: 0, bottom: 0,
     background: "rgba(0,0,0,0.5)",
     display: "flex",
-    alignItems: "center",
+    alignItems: "left",
     justifyContent: "center",
     zIndex: 1000,
 };
 
 const modalContentStyles: React.CSSProperties = {
-    background: "var(--background-color)",
+    position: "absolute",
+    background: "#5900CE",
     padding: "2rem",
     borderRadius: "8px",
     minWidth: "300px",
     maxWidth: "90vw",
+    color: "#19DCB9"
 };
 
 // TODO IMPORTANT (II) -> fix styling move it to separate css use theme colors for example `var(--primary-color)`
--- src/components/modal/SavingsAccountForm.tsx
+++ src/components/modal/SavingsAccountForm.tsx
@@ -5,26 +5,44 @@ interface Props {
     onSave: (account: SavingsAccount) => void;
 }
 
-export default function SavingsAccountForm({ onSave } : Props) {
+export default function SavingsAccountForm({onSave}: Props) {
     const [name, setName] = useState("");
+    const [rate, setRate] = useState<number | undefined>(undefined);
+    const [amount, setAmount] = useState(0);
 
     function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
         const newAccount = {
             title: name,
-            rate: 0.01,
-            amount: 100
+            rate,
+            amount
         } as SavingsAccount;
         onSave(newAccount);
     }
 
     return (
-        // TODO make it full form (it doesnt cover all of the account props)
+        // TODO make it full form (it doesnt cover all of the account props) DONE
         <form onSubmit={handleSubmit}>
             <label>
                 Account name:
                 <input value={name} required={true}
-                        onChange={e=> setName(e.target.value)} />
+                       onChange={e => setName(e.target.value)}/>
+            </label>
+            <label>
+                Account Rate:
+                <input
+                    type="number"
+                    step="0.01"
+                    value={rate === undefined ? "" : rate} required={true}
+                    onChange={e => setRate(e.target.value === "" ? undefined : Number(e.target.value))}/>
+            </label>
+            <label>
+                Amount:
+                <input
+                    type="number"
+                    step="0.1"
+                    value={amount}
+                    onChange={e => setAmount(Number(e.target.value))}/>
             </label>
             <button type="submit">Save</button>
         </form>
