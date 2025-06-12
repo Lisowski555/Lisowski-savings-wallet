1e4c64f85233282cc6e76fdf4223056e24448726
Lisowski555 <lisowski.rafal555@gmail.com>
2025-06-13 00:20:31 +0200

    wip 1, modified SVG, updated TODOs

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,6 +5,9 @@ import type {SavingsAccount, SavingsDeposit, Wallet} from "../types/Wallet.ts";
 import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 import {fetchWallet, saveWallet} from "../api/wallet.ts";
 
+//TODO IMPORTANT ZRÓB LOGIN SCREEN I OGARNIJ LOGOWANIE DO STRONY! (istotne przy hybryddowej)
+//TODO IMPORTANT ogarnąć plik co zrobi z tego prawdziwe PWA (https://web.dev/learn/pwa/web-app-manifest?hl=pl) PLIK MANIFESTU
+
 
 function Dashboard() {
     const queryClient = useQueryClient();
